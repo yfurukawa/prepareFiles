e75967e1a6744d6fc749941b3669f8dd2edf6873
Yoshihiro Furukawa <yoshihiro.furukawa@nifty.com>
2013-08-13 13:32:06 +0900

    ClassFileMakerクラスのprotected属性の値を覗くSpyクラスの追加

---
+++ test/mocks/ClassFileMakerSpy.cpp
@@ -0,0 +1,25 @@
+/*
+ * ClassFileMakerSpy.cpp
+ *
+ *  Created on: 2013/08/13
+ *      Author: furukawayoshihiro
+ */
+
+#include "ClassFileMakerSpy.h"
+
+ClassFileMakerSpy::ClassFileMakerSpy() {
+	// TODO 自動生成されたコンストラクター・スタブ
+
+}
+
+ClassFileMakerSpy::ClassFileMakerSpy(std::string name) : ClassFileMaker(name) {
+
+}
+
+ClassFileMakerSpy::~ClassFileMakerSpy() {
+	// TODO Auto-generated destructor stub
+}
+
+const std::string ClassFileMakerSpy::getHeaderName() {
+	return headerName_;
+}
---
+++ test/mocks/ClassFileMakerSpy.h
@@ -0,0 +1,22 @@
+/*
+ * ClassFileMakerSpy.h
+ *
+ *  Created on: 2013/08/13
+ *      Author: furukawayoshihiro
+ */
+
+#ifndef CLASSFILEMAKERSPY_H_
+#define CLASSFILEMAKERSPY_H_
+
+#include "ClassFileMaker.h"
+
+class ClassFileMakerSpy: public ClassFileMaker {
+private:
+	ClassFileMakerSpy();
+public:
+	ClassFileMakerSpy(std::string name);
+	virtual ~ClassFileMakerSpy();
+	const std::string getHeaderName();
+};
+
+#endif /* CLASSFILEMAKERSPY_H_ */
