c1344562a1923ded0615dcd1da03d656be65ae29
Yoshihiro Furukawa <yoshihiro.furukawa@nifty.com>
2013-08-22 22:11:36 +0900

    Refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,47 @@
 #include "ClassFileMaker.h"
 #include "TestClassFileMaker.h"
 #include "FileOutputter.h"
+#include "FileMakerList.h"
+
+ClassFileMaker* createClassFileMaker(std::string);
+TestClassFileMaker* createTestClassFileMaker(std::string);
+void buildClassList(FileMakerList*, std::vector<std::string>);
 
 int main(int argc, char* argv[]) {
-	CommandLineArgumentsParser* parser = new CommandLineArgumentsParser();
 
 	try {
-		std::vector<std::string>classes = parser->parseArguments(argc, argv);
+		CommandLineArgumentsParser* parser = new CommandLineArgumentsParser();
+		std::vector<std::string> classes = parser->parseArguments(argc, argv);
 
-		for(std::vector<std::string>::iterator classesItr = classes.begin(); classesItr != classes.end(); ++classesItr) {
-			ClassFileMaker* fileMaker = new ClassFileMaker(*classesItr);
-			TestClassFileMaker* testFileMaker = new TestClassFileMaker(*classesItr);
-			fileMaker->setOutputter(new FileOutputter());
-			fileMaker->createFiles();
+		FileMakerList* list = new FileMakerList();
+		buildClassList(list, classes);
+		list->createFiles();
 
-			testFileMaker->setOutputter(new FileOutputter());
-			testFileMaker->createFiles();
-			delete fileMaker;
-			delete testFileMaker;
-		}
+		delete list;
 		delete parser;
 		return 0;
 	}
 	catch(std::invalid_argument& e) {
-		std::cout << "Usage:" << std::endl;
+		std::cout << "Usage: prepareFiles className еее" << std::endl;
 		exit (-1);
 	}
 }
 
+ClassFileMaker* createClassFileMaker(std::string name){
+	ClassFileMaker* fileMaker = new ClassFileMaker(name);
+	fileMaker->setOutputter(new FileOutputter());
+	return fileMaker;
+}
 
+TestClassFileMaker* createTestClassFileMaker(std::string name){
+	TestClassFileMaker* testFileMaker = new TestClassFileMaker(name);
+	testFileMaker->setOutputter(new FileOutputter());
+	return testFileMaker;
+}
+
+void buildClassList(FileMakerList* list, std::vector<std::string> classes){
+	for(std::vector<std::string>::iterator classesItr = classes.begin(); classesItr != classes.end(); ++classesItr) {
+		list->addClass(createClassFileMaker(*classesItr));
+		list->addTestClass(createTestClassFileMaker(*classesItr));
+	}
+}
