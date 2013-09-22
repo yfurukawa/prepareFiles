7073b3585415884977aefa753b829d26fb52d7d2
Yoshihiro Furukawa <yoshihiro.furukawa@nifty.com>
2013-09-22 14:37:48 +0900

    コンテンツのテストをするためのSpyクラスの追加

---
+++ test/mocks/TestMainMakerSpy.cpp
@@ -0,0 +1,18 @@
+/*
+ * TestMainMakerSpy.cpp
+ *
+ *  Created on: 2013/09/22
+ *      Author: furukawayoshihiro
+ */
+
+#include "TestMainMakerSpy.h"
+
+TestMainMakerSpy::TestMainMakerSpy() {
+	// TODO 自動生成されたコンストラクター・スタブ
+
+}
+
+TestMainMakerSpy::~TestMainMakerSpy() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ test/mocks/TestMainMakerSpy.h
@@ -0,0 +1,19 @@
+/*
+ * TestMainMakerSpy.h
+ *
+ *  Created on: 2013/09/22
+ *      Author: furukawayoshihiro
+ */
+
+#ifndef TESTMAINMAKERSPY_H_
+#define TESTMAINMAKERSPY_H_
+
+#include "TestMainMaker.h"
+
+class TestMainMakerSpy: public TestMainMaker {
+public:
+	TestMainMakerSpy();
+	virtual ~TestMainMakerSpy();
+};
+
+#endif /* TESTMAINMAKERSPY_H_ */
