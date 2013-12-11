65e3ce2e56d820f8f28b2d660d876dc2deed7dd9
yfurukawa <yoshihiro.furukawanifty.com>
2013-12-11 17:14:36 +0900

    Doxygen用のコメントを追加

--- include/ClassFileMakerFactory.h
+++ include/ClassFileMakerFactory.h
@@ -1,5 +1,9 @@
 /**
- * ClassFileMakerFactory.h
+ * ClassFileMakerFactory.h<br>
+ * スケルトンコードを生成するFileMakerオブジェクトのファクトリ
+ *
+ * 言語に合ったFileMakerオブジェクトをインスタンス化し、
+ * FileMakerListに追加する
  *
  *  Created on: 2013/09/22
  *      Author: furukawayoshihiro
@@ -17,18 +21,42 @@ class IMakefileCreator;
 
 class ClassFileMakerFactory {
 private:
+	//! デフォルトコンストラクタ
 	ClassFileMakerFactory();
 
 public:
+	//! コンストラクタ
+	/*!
+	 * \param parser コマンドライン引数のパーサ
+	 */
 	ClassFileMakerFactory(CommandLineArgumentsParser* parser);
+	//! デストラクタ
 	virtual ~ClassFileMakerFactory();
+	//! FileMakerを生成し、FileMakerListに追加する
+	/*!
+	 * \param list FileMakerListオブジェクト
+	 */
 	void buildClassList(FileMakerList* list);
 
 protected:
-	CommandLineArgumentsParser* parser_;
-	std::vector<std::string> classes_;
+	CommandLineArgumentsParser* parser_; //!< コマンドライン引数のパーサオブジェクト
+	std::vector<std::string> classes_; //!< コマンドライン引数で渡されたクラスの一覧
+
+	//! 出力用オブジェクトがセットされたFileMakerオブジェクトを生成する
+	/*!
+	 * \param className クラス名
+	 * \return FileMakerオブジェクト
+	 */
 	template <typename T> IClassFileMaker* createFileMaker(std::string className);
+	//! FileMakerオブジェクトをFileMakerListに追加する
+	/*!
+	 * \param list FileMakerListオブジェクト
+	 */
 	template <typename T> void appendClassName( FileMakerList*& list );
+	//! テストクラスのFileMakerオブジェクトをFileMakerListに追加する
+	/*!
+	 * \param list FileMakerListオブジェクト
+	 */
 	template <typename T> void appendTestClassName( FileMakerList*& list );
 };
 
