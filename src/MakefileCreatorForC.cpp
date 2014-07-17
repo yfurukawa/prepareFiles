c7ef53c6f37f2f388e29e72f029f136e7df4fa68
yfurukawa <yoshihiro.furukawanifty.com>
2014-07-17 14:30:59 +0900

    C言語用のMakefileなのに、mainの拡張子がcppになっていたので修正した。

--- src/MakefileCreatorForC.cpp
+++ src/MakefileCreatorForC.cpp
@@ -12,78 +12,78 @@ MakefileCreatorForC::MakefileCreatorForC()  : outputter_(NULL), inputter_(NULL),
 }
 
 MakefileCreatorForC::~MakefileCreatorForC() {
-	// TODO Auto-generated destructor stub
-	if( outputter_ != NULL ){
-		delete outputter_;
-		outputter_ = NULL;
-	}
-	if( inputter_ != NULL ){
-		delete inputter_;
-		inputter_ = NULL;
-	}
+  // TODO Auto-generated destructor stub
+  if( outputter_ != NULL ){
+    delete outputter_;
+    outputter_ = NULL;
+  }
+  if( inputter_ != NULL ){
+    delete inputter_;
+    inputter_ = NULL;
+  }
 }
 
 MakefileCreatorForC::MakefileCreatorForC(std::string targetName)  : outputter_(NULL), inputter_(NULL), targetName_(targetName), targetNameForTest_(targetName+"Test") {
 }
 
 void MakefileCreatorForC::setOutputter(IOutputter* outputter) {
-	outputter_ = outputter;
+  outputter_ = outputter;
 }
 
 void MakefileCreatorForC::setInputter(IInputter* inputter) {
-	inputter_ = inputter;
-	subCreator_.setInputter(inputter);
+  inputter_ = inputter;
+  subCreator_.setInputter(inputter);
 }
 
 void MakefileCreatorForC::createFiles(const std::string& sourceClasses,
-		const std::string& sourceObjects, const std::string& testClasses,
-		const std::string& testObjects) {
-	outputter_->outputContents("src/Makefile", createMakefileContents());
-	subCreator_.setOutputter(outputter_);
-	subCreator_.createFiles( sourceClasses, sourceObjects, testClasses, testObjects );
+    const std::string& sourceObjects, const std::string& testClasses,
+    const std::string& testObjects) {
+  outputter_->outputContents("src/Makefile", createMakefileContents());
+  subCreator_.setOutputter(outputter_);
+  subCreator_.createFiles( sourceClasses, sourceObjects, testClasses, testObjects );
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
 std::string MakefileCreatorForC::createMakefileContents() {
 
-	std::string contents;
-	contents  = "CC = gcc\n";
-	contents += "RM = rm -f\n";
-	contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
-	contents += "LIB_DIR = -L/usr/lib -L/usr/local/lib\n";
-	contents += "LIB =\n";
-	contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
-	contents += "TARGET = " + targetName_ + "\n";
-	contents += "include productionSources.mk\n";
-	contents += "include productionObjects.mk\n";
-	contents += "\n";
-	contents += "all: $(OBJ) main.o\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(OBJ) main.o";
-	contents += "\n";
-	contents += "$(OBJ): $(SRC)\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
-	contents += "\n";
-	contents += "main.o: main.cpp\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.cpp";
-	contents += "\n";
-	contents += ".PHONY: clean\n";
-	contents += "clean:\n";
-	contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
-	contents += "\n";
-	contents += "\n";
-	contents += "TEST_CC = g++\n";
-	contents += "TEST_TARGET = " + targetNameForTest_ + "\n";
-	contents += "TEST_INCLUDE = -I../test -I.\n";
-	contents += "TEST_LIB = -lgtest\n";
-	contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
-	contents += "include testSources.mk\n";
-	contents += "include testObjects.mk\n";
-	contents += "\n";
-	contents += "test: $(OBJ) $(TEST_OBJ)\n";
-	contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(OBJ) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
-	contents += "\n";
-	contents += "$(TEST_OBJ): $(TEST_SRC)\n";
-	contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
+  std::string contents;
+  contents  = "CC = gcc\n";
+  contents += "RM = rm -f\n";
+  contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
+  contents += "LIB_DIR = -L/usr/lib -L/usr/local/lib\n";
+  contents += "LIB =\n";
+  contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
+  contents += "TARGET = " + targetName_ + "\n";
+  contents += "include productionSources.mk\n";
+  contents += "include productionObjects.mk\n";
+  contents += "\n";
+  contents += "all: $(OBJ) main.o\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(OBJ) main.o";
+  contents += "\n";
+  contents += "$(OBJ): $(SRC)\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
+  contents += "\n";
+  contents += "main.o: main.c\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.c\n";
+  contents += "\n";
+  contents += ".PHONY: clean\n";
+  contents += "clean:\n";
+  contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
+  contents += "\n";
+  contents += "\n";
+  contents += "TEST_CC = g++\n";
+  contents += "TEST_TARGET = " + targetNameForTest_ + "\n";
+  contents += "TEST_INCLUDE = -I../test -I.\n";
+  contents += "TEST_LIB = -lgtest\n";
+  contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
+  contents += "include testSources.mk\n";
+  contents += "include testObjects.mk\n";
+  contents += "\n";
+  contents += "test: $(OBJ) $(TEST_OBJ)\n";
+  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(OBJ) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
+  contents += "\n";
+  contents += "$(TEST_OBJ): $(TEST_SRC)\n";
+  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
 
-	return contents;
+  return contents;
 }
--- test/MakefileCreatorForCTest.cpp
+++ test/MakefileCreatorForCTest.cpp
@@ -11,77 +11,77 @@
 #include "mocks/OutputterMock.h"
 
 MakefileCreatorForCTest::MakefileCreatorForCTest() : sut(NULL){
-	// TODO �����������ꂽ�R���X�g���N�^�[�E�X�^�u
-	sut = new MakefileCreatorForCSpy("targetName");
+  // TODO �����������ꂽ�R���X�g���N�^�[�E�X�^�u
+  sut = new MakefileCreatorForCSpy("targetName");
 }
 
 MakefileCreatorForCTest::~MakefileCreatorForCTest() {
-	// TODO Auto-generated destructor stub
-	delete sut;
+  // TODO Auto-generated destructor stub
+  delete sut;
 }
 
 TEST_F(MakefileCreatorForCTest, setOutputter) {
-	IOutputter* outputter = new OutputterMock();
-	sut->setOutputter( outputter );
+  IOutputter* outputter = new OutputterMock();
+  sut->setOutputter( outputter );
 
-	EXPECT_EQ(outputter, sut->getOutputter());
+  EXPECT_EQ(outputter, sut->getOutputter());
 }
 
 TEST_F(MakefileCreatorForCTest, createMakefile) {
-	OutputterMock* outputter = new OutputterMock();
-	sut->setOutputter( outputter );
-	OutputterMock* inputter = new OutputterMock();
-	sut->setInputter( inputter );
+  OutputterMock* outputter = new OutputterMock();
+  sut->setOutputter( outputter );
+  OutputterMock* inputter = new OutputterMock();
+  sut->setInputter( inputter );
 
-	std::string expected = createExpectedMakefileContents();
+  std::string expected = createExpectedMakefileContents();
 
-	sut->createFiles("Hoge.cpp Fuga.cpp Foo.cpp", "Hoge.o Fuga.o Foo.o", "../test/HogeTest.cpp ../test/FugaTest.cpp ../test/FooTest.cpp", "HogeTest.o FugaTest.o FooTest.o");
-	EXPECT_EQ(expected, outputter->getContents(0));
-	EXPECT_EQ("SRC = Hoge.cpp Fuga.cpp Foo.cpp\n", outputter->getContents(1));
-	EXPECT_EQ("OBJ = Hoge.o Fuga.o Foo.o\n", outputter->getContents(2));
-	EXPECT_EQ("TEST_SRC = ../test/testMain.cpp ../test/HogeTest.cpp ../test/FugaTest.cpp ../test/FooTest.cpp\n", outputter->getContents(3));
-	EXPECT_EQ("TEST_OBJ = testMain.o HogeTest.o FugaTest.o FooTest.o\n", outputter->getContents(4));
+  sut->createFiles("Hoge.c Fuga.c Foo.c", "Hoge.o Fuga.o Foo.o", "../test/HogeTest.c ../test/FugaTest.c ../test/FooTest.c", "HogeTest.o FugaTest.o FooTest.o");
+  EXPECT_EQ(expected, outputter->getContents(0));
+  EXPECT_EQ("SRC = Hoge.c Fuga.c Foo.c\n", outputter->getContents(1));
+  EXPECT_EQ("OBJ = Hoge.o Fuga.o Foo.o\n", outputter->getContents(2));
+  EXPECT_EQ("TEST_SRC = ../test/testMain.cpp ../test/HogeTest.c ../test/FugaTest.c ../test/FooTest.c\n", outputter->getContents(3));
+  EXPECT_EQ("TEST_OBJ = testMain.o HogeTest.o FugaTest.o FooTest.o\n", outputter->getContents(4));
 }
 
 std::string MakefileCreatorForCTest::createExpectedMakefileContents() {
-	std::string contents;
-	contents  = "CC = gcc\n";
-	contents += "RM = rm -f\n";
-	contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
-	contents += "LIB_DIR = -L/usr/lib -L/usr/local/lib\n";
-	contents += "LIB =\n";
-	contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
-	contents += "TARGET = targetName\n";
-	contents += "include productionSources.mk\n";
-	contents += "include productionObjects.mk\n";
-	contents += "\n";
-	contents += "all: $(OBJ) main.o\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(OBJ) main.o";
-	contents += "\n";
-	contents += "$(OBJ): $(SRC)\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
-	contents += "\n";
-	contents += "main.o: main.cpp\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.cpp";
-	contents += "\n";
-	contents += ".PHONY: clean\n";
-	contents += "clean:\n";
-	contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
-	contents += "\n";
-	contents += "\n";
-	contents += "TEST_CC = g++\n";
-	contents += "TEST_TARGET = targetNameTest\n";
-	contents += "TEST_INCLUDE = -I../test -I.\n";
-	contents += "TEST_LIB = -lgtest\n";
-	contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
-	contents += "include testSources.mk\n";
-	contents += "include testObjects.mk\n";
-	contents += "\n";
-	contents += "test: $(OBJ) $(TEST_OBJ)\n";
-	contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(OBJ) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
-	contents += "\n";
-	contents += "$(TEST_OBJ): $(TEST_SRC)\n";
-	contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
+  std::string contents;
+  contents  = "CC = gcc\n";
+  contents += "RM = rm -f\n";
+  contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
+  contents += "LIB_DIR = -L/usr/lib -L/usr/local/lib\n";
+  contents += "LIB =\n";
+  contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
+  contents += "TARGET = targetName\n";
+  contents += "include productionSources.mk\n";
+  contents += "include productionObjects.mk\n";
+  contents += "\n";
+  contents += "all: $(OBJ) main.o\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(OBJ) main.o";
+  contents += "\n";
+  contents += "$(OBJ): $(SRC)\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
+  contents += "\n";
+  contents += "main.o: main.c\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.c\n";
+  contents += "\n";
+  contents += ".PHONY: clean\n";
+  contents += "clean:\n";
+  contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
+  contents += "\n";
+  contents += "\n";
+  contents += "TEST_CC = g++\n";
+  contents += "TEST_TARGET = targetNameTest\n";
+  contents += "TEST_INCLUDE = -I../test -I.\n";
+  contents += "TEST_LIB = -lgtest\n";
+  contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
+  contents += "include testSources.mk\n";
+  contents += "include testObjects.mk\n";
+  contents += "\n";
+  contents += "test: $(OBJ) $(TEST_OBJ)\n";
+  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(OBJ) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
+  contents += "\n";
+  contents += "$(TEST_OBJ): $(TEST_SRC)\n";
+  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
 
-	return contents;
+  return contents;
 }
