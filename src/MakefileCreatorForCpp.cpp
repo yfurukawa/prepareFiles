60e9260f42adbcebdda20cf992f92c29f96574bd
yfurukawa <yoshihiro.furukawanifty.com>
2014-11-26 09:43:21 +0900

    c++用のユニットテストをビルドするためのMakefileのマクロが展開されていなかったので修正した。

--- src/MakefileCreatorForCpp.cpp
+++ src/MakefileCreatorForCpp.cpp
@@ -16,73 +16,73 @@ MakefileCreatorForCpp::MakefileCreatorForCpp( std::string name ) : outputter_(NU
 }
 
 MakefileCreatorForCpp::~MakefileCreatorForCpp() {
-	// TODO Auto-generated destructor stub
-	if( outputter_ != NULL ){
-		delete outputter_;
-		outputter_ = NULL;
-	}
-	if( inputter_ != NULL ){
-		delete inputter_;
-		inputter_ = NULL;
-	}
+  // TODO Auto-generated destructor stub
+  if( outputter_ != NULL ){
+    delete outputter_;
+    outputter_ = NULL;
+  }
+  if( inputter_ != NULL ){
+    delete inputter_;
+    inputter_ = NULL;
+  }
 }
 
 void MakefileCreatorForCpp::setOutputter(IOutputter* outputter) {
-	outputter_ = outputter;
-	subCreator.setOutputter(outputter_);
+  outputter_ = outputter;
+  subCreator.setOutputter(outputter_);
 }
 
 void MakefileCreatorForCpp::setInputter(IInputter* inputter) {
-	inputter_ = inputter;
-	subCreator.setInputter(inputter);
+  inputter_ = inputter;
+  subCreator.setInputter(inputter);
 }
 
 void MakefileCreatorForCpp::createFiles(const std::string& sourceClasses,
-		const std::string& sourceObjects, const std::string& testClasses, const std::string& testObjects) {
-	outputter_->outputContents("src/Makefile", createMakefileContents());
-	subCreator.createFiles( sourceClasses, sourceObjects, testClasses, testObjects );
+    const std::string& sourceObjects, const std::string& testClasses, const std::string& testObjects) {
+  outputter_->outputContents("src/Makefile", createMakefileContents());
+  subCreator.createFiles( sourceClasses, sourceObjects, testClasses, testObjects );
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 std::string MakefileCreatorForCpp::createMakefileContents() {
 
-	std::string contents;
-	contents  = "CC = g++\n";
-	contents += "RM = rm -f\n";
-	contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
-	contents += "LIB_DIR = -L/usr/lib -L/usr/local/lib\n";
-	contents += "LIB = -lpthread\n";
-	contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
-	contents += "TARGET = " + targetName_ + "\n";
-	contents += "include productionSources.mk\n";
-	contents += "\n";
-	contents += "all: $(SRC:.cpp=.o) main.o\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(SRC:.cpp=.o) main.o";
-	contents += "\n";
-	contents += "$(SRC:.cpp=.o): $(SRC)\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
-	contents += "\n";
-	contents += "main.o: main.cpp\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.cpp";
-	contents += "\n";
-	contents += ".PHONY: clean\n";
-	contents += "clean:\n";
-	contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
-	contents += "\n";
-	contents += "\n";
-	contents += "TEST_TARGET = " + targetNameForTest_ + "\n";
-	contents += "TEST_INCLUDE = -I../test -I.\n";
-	contents += "TEST_LIB = -lgtest\n";
-	contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
-	contents += "include testSources.mk\n";
-	contents += "include testObjects.mk\n";
-	contents += "\n";
-	contents += "test: $(OBJ) $(TEST_OBJ)\n";
-	contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(OBJ) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
-	contents += "\n";
-	contents += "$(TEST_OBJ): $(TEST_SRC)\n";
-	contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
+  std::string contents;
+  contents  = "CC = g++\n";
+  contents += "RM = rm -f\n";
+  contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
+  contents += "LIB_DIR = -L/usr/lib64 -L/usr/local/lib64\n";
+  contents += "LIB = -lpthread\n";
+  contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
+  contents += "TARGET = " + targetName_ + "\n";
+  contents += "include productionSources.mk\n";
+  contents += "\n";
+  contents += "all: $(SRC:.cpp=.o) main.o\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(SRC:.cpp=.o) main.o";
+  contents += "\n";
+  contents += "$(SRC:.cpp=.o): $(SRC)\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
+  contents += "\n";
+  contents += "main.o: main.cpp\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.cpp";
+  contents += "\n";
+  contents += ".PHONY: clean\n";
+  contents += "clean:\n";
+  contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
+  contents += "\n";
+  contents += "\n";
+  contents += "TEST_TARGET = " + targetNameForTest_ + "\n";
+  contents += "TEST_INCLUDE = -I../test -I.\n";
+  contents += "TEST_LIB = -lgtest\n";
+  contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
+  contents += "include testSources.mk\n";
+  contents += "include testObjects.mk\n";
+  contents += "\n";
+  contents += "test: $(SRC:.cpp=.o) $(TEST_OBJ)\n";
+  contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(SRC:.cpp=.o) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
+  contents += "\n";
+  contents += "$(TEST_OBJ): $(TEST_SRC)\n";
+  contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
 
-	return contents;
+  return contents;
 }
 
--- test/MakefileCreatorForCppTest.cpp
+++ test/MakefileCreatorForCppTest.cpp
@@ -11,75 +11,73 @@
 #include "mocks/OutputterMock.h"
 
 MakefileCreatorForCppTest::MakefileCreatorForCppTest() {
-	// TODO
-	sut = new MakefileCreatorForCppSpy("targetName");
+  sut = new MakefileCreatorForCppSpy("targetName");
 }
 
 MakefileCreatorForCppTest::~MakefileCreatorForCppTest() {
-	// TODO Auto-generated destructor stub
-	delete sut;
+  delete sut;
 }
 
 TEST_F(MakefileCreatorForCppTest, setOutputter) {
-	IOutputter* outputter = new OutputterMock();
-	sut->setOutputter( outputter );
+  IOutputter* outputter = new OutputterMock();
+  sut->setOutputter( outputter );
 
-	EXPECT_EQ(outputter, sut->getOutputter());
+  EXPECT_EQ(outputter, sut->getOutputter());
 }
 
 TEST_F(MakefileCreatorForCppTest, createMakefile) {
-	OutputterMock* outputter = new OutputterMock();
-	sut->setOutputter( outputter );
-	OutputterMock* inputter = new OutputterMock();
-	sut->setInputter( inputter );
+  OutputterMock* outputter = new OutputterMock();
+  sut->setOutputter( outputter );
+  OutputterMock* inputter = new OutputterMock();
+  sut->setInputter( inputter );
 
-	std::string expected = createExpectedMakefileContents();
+  std::string expected = createExpectedMakefileContents();
 
-	sut->createFiles("Hoge.cpp Fuga.cpp Foo.cpp", "Hoge.o Fuga.o Foo.o", "../test/HogeTest.cpp ../test/FugaTest.cpp ../test/FooTest.cpp", "HogeTest.o FugaTest.o FooTest.o");
-	EXPECT_EQ(expected, outputter->getContents(0));
-	EXPECT_EQ("SRC = Hoge.cpp Fuga.cpp Foo.cpp\n", outputter->getContents(1));
-	EXPECT_EQ("OBJ = Hoge.o Fuga.o Foo.o\n", outputter->getContents(2));
-	EXPECT_EQ("TEST_SRC = ../test/testMain.cpp ../test/HogeTest.cpp ../test/FugaTest.cpp ../test/FooTest.cpp\n", outputter->getContents(3));
-	EXPECT_EQ("TEST_OBJ = testMain.o HogeTest.o FugaTest.o FooTest.o\n", outputter->getContents(4));
+  sut->createFiles("Hoge.cpp Fuga.cpp Foo.cpp", "Hoge.o Fuga.o Foo.o", "../test/HogeTest.cpp ../test/FugaTest.cpp ../test/FooTest.cpp", "HogeTest.o FugaTest.o FooTest.o");
+  EXPECT_EQ(expected, outputter->getContents(0));
+  EXPECT_EQ("SRC = Hoge.cpp Fuga.cpp Foo.cpp\n", outputter->getContents(1));
+  EXPECT_EQ("OBJ = Hoge.o Fuga.o Foo.o\n", outputter->getContents(2));
+  EXPECT_EQ("TEST_SRC = ../test/testMain.cpp ../test/HogeTest.cpp ../test/FugaTest.cpp ../test/FooTest.cpp\n", outputter->getContents(3));
+  EXPECT_EQ("TEST_OBJ = testMain.o HogeTest.o FugaTest.o FooTest.o\n", outputter->getContents(4));
 }
 
 std::string MakefileCreatorForCppTest::createExpectedMakefileContents() {
-	std::string contents;
-	contents  = "CC = g++\n";
-	contents += "RM = rm -f\n";
-	contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
-	contents += "LIB_DIR = -L/usr/lib -L/usr/local/lib\n";
-	contents += "LIB = -lpthread\n";
-	contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
-	contents += "TARGET = targetName\n";
-	contents += "include productionSources.mk\n";
-	contents += "\n";
-	contents += "all: $(SRC:.cpp=.o) main.o\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(SRC:.cpp=.o) main.o";
-	contents += "\n";
-	contents += "$(SRC:.cpp=.o): $(SRC)\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
-	contents += "\n";
-	contents += "main.o: main.cpp\n";
-	contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.cpp";
-	contents += "\n";
-	contents += ".PHONY: clean\n";
-	contents += "clean:\n";
-	contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
-	contents += "\n";
-	contents += "\n";
-	contents += "TEST_TARGET = targetNameTest\n";
-	contents += "TEST_INCLUDE = -I../test -I.\n";
-	contents += "TEST_LIB = -lgtest\n";
-	contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
-	contents += "include testSources.mk\n";
-	contents += "include testObjects.mk\n";
-	contents += "\n";
-	contents += "test: $(OBJ) $(TEST_OBJ)\n";
-	contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(OBJ) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
-	contents += "\n";
-	contents += "$(TEST_OBJ): $(TEST_SRC)\n";
-	contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
+  std::string contents;
+  contents  = "CC = g++\n";
+  contents += "RM = rm -f\n";
+  contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
+  contents += "LIB_DIR = -L/usr/lib64 -L/usr/local/lib64\n";
+  contents += "LIB = -lpthread\n";
+  contents += "OPT = -O0 -g3 -Wall -Wnon-virtual-dtor -Woverloaded-virtual -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
+  contents += "TARGET = targetName\n";
+  contents += "include productionSources.mk\n";
+  contents += "\n";
+  contents += "all: $(SRC:.cpp=.o) main.o\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(SRC:.cpp=.o) main.o";
+  contents += "\n";
+  contents += "$(SRC:.cpp=.o): $(SRC)\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
+  contents += "\n";
+  contents += "main.o: main.cpp\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c main.cpp";
+  contents += "\n";
+  contents += ".PHONY: clean\n";
+  contents += "clean:\n";
+  contents += "\t$(RM) *.o $(TARGET) $(TEST_TARGET) gmon.out *.gc* *.xml *.exe *.txt\n";
+  contents += "\n";
+  contents += "\n";
+  contents += "TEST_TARGET = targetNameTest\n";
+  contents += "TEST_INCLUDE = -I../test -I.\n";
+  contents += "TEST_LIB = -lgtest\n";
+  contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
+  contents += "include testSources.mk\n";
+  contents += "include testObjects.mk\n";
+  contents += "\n";
+  contents += "test: $(SRC:.cpp=.o) $(TEST_OBJ)\n";
+  contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(SRC:.cpp=.o) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
+  contents += "\n";
+  contents += "$(TEST_OBJ): $(TEST_SRC)\n";
+  contents += "\t$(CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
 
-	return contents;
+  return contents;
 }
