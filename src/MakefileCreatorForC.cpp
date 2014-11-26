86046ef1081f930754dbe1255a475a436a05bbb1
yfurukawa <yoshihiro.furukawanifty.com>
2014-11-26 11:08:28 +0900

    c用Makefileもマクロ展開する様に修正。

--- src/MakefileCreatorForC.cpp
+++ src/MakefileCreatorForC.cpp
@@ -50,17 +50,16 @@ std::string MakefileCreatorForC::createMakefileContents() {
   contents  = "CC = gcc\n";
   contents += "RM = rm -f\n";
   contents += "INCLUDE = -I/usr/incude -I/usr/local/include\n";
-  contents += "LIB_DIR = -L/usr/lib -L/usr/local/lib\n";
-  contents += "LIB =\n";
+  contents += "LIB_DIR = -L/usr/lib64 -L/usr/local/lib64\n";
+  contents += "LIB = -lpthread\n";
   contents += "OPT = -O0 -g3 -Wall -fmessage-length=0 -fprofile-arcs -ftest-coverage\n";
   contents += "TARGET = " + targetName_ + "\n";
   contents += "include productionSources.mk\n";
-  contents += "include productionObjects.mk\n";
   contents += "\n";
-  contents += "all: $(OBJ) main.o\n";
-  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(OBJ) main.o";
+  contents += "all: $(SRC:.cpp=.o) main.o\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(SRC:.cpp=.o) main.o";
   contents += "\n";
-  contents += "$(OBJ): $(SRC)\n";
+  contents += "$(SRC:.cpp=.o): $(SRC)\n";
   contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
   contents += "\n";
   contents += "main.o: main.c\n";
@@ -77,12 +76,11 @@ std::string MakefileCreatorForC::createMakefileContents() {
   contents += "TEST_LIB = -lgtest\n";
   contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
   contents += "include testSources.mk\n";
-  contents += "include testObjects.mk\n";
   contents += "\n";
-  contents += "test: $(OBJ) $(TEST_OBJ)\n";
-  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(OBJ) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
+  contents += "test: $(SRC:.cpp=.o) $(TEST_SRC:.cpp=.o)\n";
+  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(SRC:.cpp=.o) $(TEST_SRC:.cpp=.o) $(LIB) $(TEST_LIB)\n";
   contents += "\n";
-  contents += "$(TEST_OBJ): $(TEST_SRC)\n";
+  contents += "$(TEST_SRC:.cpp=.o): $(TEST_SRC)\n";
   contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
 
   return contents;
