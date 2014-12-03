f8ca80b988abe3b4699c9cc08b2ba6a8e5c3c716
yfurukawa <y-furukawa@churyo.co.jp>
2014-12-03 10:54:00 +0900

    マクロの展開を間違えていたので修正

--- test/MakefileCreatorForCTest.cpp
+++ test/MakefileCreatorForCTest.cpp
@@ -52,10 +52,10 @@ std::string MakefileCreatorForCTest::createExpectedMakefileContents() {
   contents += "TARGET = targetName\n";
   contents += "include productionSources.mk\n";
   contents += "\n";
-  contents += "all: $(SRC:.cpp=.o) main.o\n";
-  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(SRC:.cpp=.o) main.o";
+  contents += "all: $(SRC:.c=.o) main.o\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -o $(TARGET) $(SRC:.c=.o) main.o";
   contents += "\n";
-  contents += "$(SRC:.cpp=.o): $(SRC)\n";
+  contents += "$(SRC:.c=.o): $(SRC)\n";
   contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
   contents += "\n";
   contents += "main.o: main.c\n";
@@ -72,11 +72,15 @@ std::string MakefileCreatorForCTest::createExpectedMakefileContents() {
   contents += "TEST_LIB = -lgtest\n";
   contents += "TEST_OPT = -O0 -g3 -Wall -fmessage-length=0 -pg -fprofile-arcs -ftest-coverage\n";
   contents += "include testSources.mk\n";
+  contents += "include testObjects.mk\n";
   contents += "\n";
-  contents += "test: $(SRC:.cpp=.o) $(TEST_SRC:.cpp=.o)\n";
-  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(SRC:.cpp=.o) $(TEST_SRC:.cpp=.o) $(LIB) $(TEST_LIB)\n";
+  contents += "test: $(SRC:.c=.o) $(TEST_OBJ)\n";
+  contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(TEST_OPT) -o $(TEST_TARGET) $(SRC:.c=.o) $(TEST_OBJ) $(LIB) $(TEST_LIB)\n";
   contents += "\n";
-  contents += "$(TEST_SRC:.cpp=.o): $(TEST_SRC)\n";
+  contents += "$(SRC:.c=.o): $(SRC)\n";
+  contents += "\t$(CC) $(INCLUDE) $(LIB_DIR) $(OPT) -c $(SRC)\n";
+  contents += "\n";
+  contents += "$(TEST_OBJ): $(TEST_SRC)\n";
   contents += "\t$(TEST_CC) $(INCLUDE) $(TEST_INCLUDE) $(LIB_DIR) $(LIB) $(TEST_LIB) $(TEST_OPT) -c $(TEST_SRC)\n";
 
   return contents;
