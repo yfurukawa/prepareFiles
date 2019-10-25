34ded4ea7706fbbb516526bf29bc05ed8cad7b4b
YFurukawa <yo4.furukawa@gmail.com>
2019-10-25 22:50:10 +0900

    sutの初期化を追加

--- src/TestClassFileMaker.cpp
+++ src/TestClassFileMaker.cpp
@@ -7,100 +7,106 @@
 
 #include "TestClassFileMaker.h"
 
-TestClassFileMaker::TestClassFileMaker() : name_(""), testTargetClassName_(""), className_(""), headerName_(""), objectName_(""), headerSkeleton_(""), cppSkeleton_(""), outputter_(NULL) {
-	// TODO
+TestClassFileMaker::TestClassFileMaker() : name_(""), testTargetClassName_(""), className_(
+        ""), headerName_(""), objectName_(""), headerSkeleton_(""), cppSkeleton_(
+        ""), outputter_(NULL) {
+    // TODO
 
 }
 
-TestClassFileMaker::TestClassFileMaker(std::string name) : name_(name+"Test"), testTargetClassName_(name), className_(name+"Test.cpp"), headerName_(name+"Test.h"), objectName_(name+"Test.o"), headerSkeleton_(""), cppSkeleton_(""), outputter_(NULL) {
+TestClassFileMaker::TestClassFileMaker(std::string name) : name_(name + "Test"), testTargetClassName_(
+        name), className_(name + "Test.cpp"), headerName_(name + "Test.h"), objectName_(
+        name + "Test.o"), headerSkeleton_(""), cppSkeleton_(""), outputter_(
+NULL) {
 }
 
 TestClassFileMaker::~TestClassFileMaker() {
-	// TODO Auto-generated destructor stub
-	if( outputter_ != NULL ){
-		delete outputter_;
-		outputter_ = NULL;
-	}
+    // TODO Auto-generated destructor stub
+    if (outputter_ != NULL) {
+        delete outputter_;
+        outputter_ = NULL;
+    }
 }
 
 void TestClassFileMaker::createFiles() {
-	createHeaderFile();
-	createImplementsFile();
+    createHeaderFile();
+    createImplementsFile();
 
-	if( isOutputterSet() ) {
-		outputter_->outputContents("test/" + headerName_, headerSkeleton_);
-		outputter_->outputContents("test/" + className_, cppSkeleton_ );
-	}
+    if (isTextWriterSet()) {
+        outputter_->outputContents("test/" + headerName_, headerSkeleton_);
+        outputter_->outputContents("test/" + className_, cppSkeleton_);
+    }
 }
 
 const std::string TestClassFileMaker::getClassName() const {
-	return "../test/"+className_;
+    return "../test/" + className_;
 }
 
 const std::string TestClassFileMaker::getName() const {
-	return name_;
+    return name_;
 }
 
 void TestClassFileMaker::setOutputter(IOutputter* outputter) {
-	outputter_ = outputter;
+    outputter_ = outputter;
 }
 
 const std::string TestClassFileMaker::getObjectName() const {
-	return objectName_;
+    return objectName_;
 }
 
 //////////////////////////////////////////////////////////////////////
 void TestClassFileMaker::createHeaderFile() {
-	std::string label(name_);
+    std::string label(name_);
 
-	transform(label.begin(), label.end(), label.begin(), ::toupper);
-	label += "_H_";
-	headerSkeleton_  = "#ifndef " + label + "\n";
-	headerSkeleton_ += "#define " + label + "\n";
-	headerSkeleton_ += "\n";
-	headerSkeleton_ += "#include <gtest/gtest.h>\n";
-	headerSkeleton_ += "#include \"" + testTargetClassName_ + ".h\"\n";
-	headerSkeleton_ += "\n";
-	headerSkeleton_ += "class " + name_ + " : public ::testing::Test {\n";
-	headerSkeleton_ += "protected:\n";
-	headerSkeleton_ += "\t" + testTargetClassName_ + "* sut;\n";
-	headerSkeleton_ += "\tvoid SetUp();\n";
-	headerSkeleton_ += "\tvoid TearDown();\n";
-	headerSkeleton_ += "\n";
-	headerSkeleton_ += "public:\n";
-	headerSkeleton_ += "\t" + name_ + "();\n";
-	headerSkeleton_ += "\tvirtual ~" + name_ + "();\n";
-	headerSkeleton_ += "\n";
-	headerSkeleton_ += "};\n";
-	headerSkeleton_ += "\n";
-	headerSkeleton_ += "#endif\n";
+    transform(label.begin(), label.end(), label.begin(), ::toupper);
+    label += "_H_";
+    headerSkeleton_ = "#ifndef " + label + "\n";
+    headerSkeleton_ += "#define " + label + "\n";
+    headerSkeleton_ += "\n";
+    headerSkeleton_ += "#include <gtest/gtest.h>\n";
+    headerSkeleton_ += "#include \"" + testTargetClassName_ + ".h\"\n";
+    headerSkeleton_ += "\n";
+    headerSkeleton_ += "class " + name_ + " : public ::testing::Test {\n";
+    headerSkeleton_ += "protected:\n";
+    headerSkeleton_ += "\t" + testTargetClassName_ + "* sut;\n";
+    headerSkeleton_ += "\tvoid SetUp();\n";
+    headerSkeleton_ += "\tvoid TearDown();\n";
+    headerSkeleton_ += "\n";
+    headerSkeleton_ += "public:\n";
+    headerSkeleton_ += "\t" + name_ + "();\n";
+    headerSkeleton_ += "\tvirtual ~" + name_ + "();\n";
+    headerSkeleton_ += "\n";
+    headerSkeleton_ += "};\n";
+    headerSkeleton_ += "\n";
+    headerSkeleton_ += "#endif\n";
 }
 
 void TestClassFileMaker::createImplementsFile() {
-	cppSkeleton_  = "#include \"" + headerName_ + "\"\n";
-	cppSkeleton_ += "\n";
-	cppSkeleton_ += name_ + "::" + name_ + "() {\n";
-	cppSkeleton_ += "\n";
-	cppSkeleton_ += "}\n";
-	cppSkeleton_ += "\n";
-	cppSkeleton_ += name_ + "::~" + name_ + "() {\n";
-	cppSkeleton_ += "\n";
-	cppSkeleton_ += "}\n";
-	cppSkeleton_ += "\n";
-	cppSkeleton_ += "void " + name_ + "::SetUp() {\n";
-	cppSkeleton_ += "\tsut = new " + testTargetClassName_ + "();\n";
-	cppSkeleton_ += "}\n";
-	cppSkeleton_ += "\n";
-	cppSkeleton_ += "void " + name_ + "::TearDown() {\n";
-	cppSkeleton_ += "\tdelete sut;\n";
-	cppSkeleton_ += "}\n";
-	cppSkeleton_ += "\n";
-	cppSkeleton_ += "TEST_F (" + name_ + ", testNameIsHere_ChangeThis) {\n";
-	cppSkeleton_ += "/* Write a test code here. */\n\n";
-	cppSkeleton_ += "}\n";
+    cppSkeleton_ = "#include <iostream>";
+    cppSkeleton_ += "#include \"" + headerName_ + "\"\n";
+    cppSkeleton_ += "\n";
+    cppSkeleton_ += name_ + "::" + name_ + "() : sut(NULL){\n";
+    cppSkeleton_ += "\n";
+    cppSkeleton_ += "}\n";
+    cppSkeleton_ += "\n";
+    cppSkeleton_ += name_ + "::~" + name_ + "() {\n";
+    cppSkeleton_ += "\n";
+    cppSkeleton_ += "}\n";
+    cppSkeleton_ += "\n";
+    cppSkeleton_ += "void " + name_ + "::SetUp() {\n";
+    cppSkeleton_ += "\tsut = new " + testTargetClassName_ + "();\n";
+    cppSkeleton_ += "}\n";
+    cppSkeleton_ += "\n";
+    cppSkeleton_ += "void " + name_ + "::TearDown() {\n";
+    cppSkeleton_ += "\tdelete sut;\n";
+    cppSkeleton_ += "}\n";
+    cppSkeleton_ += "\n";
+    cppSkeleton_ += "TEST_F (" + name_ + ", testNameIsHere_ChangeThis) {\n";
+    cppSkeleton_ += "/* Write a test code here. */\n\n";
+    cppSkeleton_ += "}\n";
 }
 
-bool TestClassFileMaker::isOutputterSet() {
-	return outputter_ != NULL;
+bool TestClassFileMaker::isTextWriterSet() {
+    return outputter_ != NULL;
 }
 
